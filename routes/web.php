b904edb40c65ffd4c192fd1e42bd11f2ea7002fb
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-03-20 23:31:58 +0100

    Set an Active Link

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/',function(){
     return view('welcome');
 });
 
-Route::get('/contact',function(){
-    return view('contact');
+Route::get('/about',function(){
+    return view('about');
 });
 // Route::get('/posts/{post}','PostsController@show');
